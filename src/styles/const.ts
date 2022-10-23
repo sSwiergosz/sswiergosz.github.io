0fcd45a43dc95cdf71432355694e40715ff51738
Szymon Świergosz <szymonswiergosz@MacBook-Pro-Szymon-2.local>
2022-10-23 22:30:21 +0200

    Cleanup styles, move colors to const file

---
+++ src/styles/const.ts
@@ -0,0 +1,5 @@
+export const mainBackgroundColor = '#011627';
+export const mainLightBackgroundColor = '#0f2435';
+export const mainFontColor = '#fff';
+export const mainHeadingColor = '#f49e4c';
+export const mainLinkColor = '#ab3428';
