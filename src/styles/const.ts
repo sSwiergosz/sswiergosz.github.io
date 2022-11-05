17b4c0123aa6411b4b33748a8550c3ae20da145e
Szymon Świergosz <szymonswiergosz@MacBook-Pro-Szymon-2.local>
2022-11-05 21:42:56 +0100

    Add next sections and include first images

--- src/styles/const.ts
+++ src/styles/const.ts
@@ -2,4 +2,4 @@ export const mainBackgroundColor = '#011627';
 export const mainLightBackgroundColor = '#0f2435';
 export const mainFontColor = '#fff';
 export const mainHeadingColor = '#f49e4c';
-export const mainLinkColor = '#ab3428';
+export const mainLinkColor = '#E78888';
