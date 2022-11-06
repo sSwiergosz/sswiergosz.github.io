a58b733f9591b4761447ed3ba0b14fb808765ace
Szymon Świergosz <szymonswiergosz@MacBook-Pro-Szymon-2.local>
2022-11-06 14:32:39 +0100

    Make changes in blog images and rewrite the existing sections

--- src/styles/const.ts
+++ src/styles/const.ts
@@ -1,5 +1,5 @@
 export const mainBackgroundColor = '#011627';
-export const mainLightBackgroundColor = '#0f2435';
+export const mainLightBackgroundColor = '#0a2942';
 export const mainFontColor = '#fff';
 export const mainHeadingColor = '#f49e4c';
 export const mainLinkColor = '#E78888';
