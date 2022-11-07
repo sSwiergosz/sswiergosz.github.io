9d4c87fc5817e4ab9096e6e2c9329351207841e1
Szymon Świergosz <szymonswiergosz@MacBook-Pro-Szymon-2.local>
2022-11-07 22:18:21 +0100

    Restore base parameter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://sswiergosz.github.io',
+	base: '/blog',
 	integrations: [mdx(), sitemap()],
 });
