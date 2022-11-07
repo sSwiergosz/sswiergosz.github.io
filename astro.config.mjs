67e7d6c4362ec1bf0afd0b298dd7c029d259b02b
Szymon Świergosz <szymonswiergosz@MacBook-Pro-Szymon-2.local>
2022-11-07 22:25:15 +0100

    Remove base parameter after renaming repository

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://sswiergosz.github.io',
-	base: '/blog',
 	integrations: [mdx(), sitemap()],
 });
